ec2890648a29ab69a90f16077ca97947fba2e27c
J.B Stepan <tonymoooon543@gmail.com>
2020-01-15 14:29:53 -0600

    2 hours

--- Website testing/scrpit.js
+++ Website testing/scrpit.js
@@ -1,14 +1,11 @@
-var audioplay = document.getElementById('audio');
-
 // This will print the date on the screen in 'demo' paragraph
 function changeP() {
   document.getElementById('demo').innerHTML = Date();
 }
 
-function playAudio() {
-  audioplay.play();
-}
-
-function pauseAudio() {
-  audioplay.pause(); 
+function promptInput() {
+  var person = prompt("Please enter your name")
+  if (person != null) {
+    document.getElementById('name').innerHTML = "Hello " + person + ", how are you doing"
+  }
 }
