41209df085be1c73476e33aeac55b3f3cf412d20
J.B Stepan <tonymoooon543@gmail.com>
2020-01-15 13:01:09 -0600

    a

--- Website testing/scrpit.js
+++ Website testing/scrpit.js
@@ -1,5 +1,14 @@
-var audio = document.getElementById('')
+var audioplay = document.getElementById('audio');
 
+// This will print the date on the screen in 'demo' paragraph
 function changeP() {
   document.getElementById('demo').innerHTML = Date();
+}
+
+function playAudio() {
+  audioplay.play();
+}
+
+function pauseAudio() {
+  audioplay.pause(); 
 }
