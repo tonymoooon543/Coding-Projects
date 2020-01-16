e9f7cee3b4876b8e3413f385cb70147c1c68e806
J.B Stepan <tonymoooon543@gmail.com>
2020-01-15 22:17:00 -0600

    a

--- Website testing/scrpit.js
+++ Website testing/scrpit.js
@@ -9,3 +9,12 @@ function promptInput() {
     document.getElementById('name').innerHTML = "Hello " + person + ", how are you doing"
   }
 }
+
+function loginForm() {
+  var x = document.getElementById('username')
+  var y = document.getElementById('password')
+
+  if (x == 'tonymoooon543' && y == 'duckey123') {
+    document.getElementById('logedIn').innerHTML = "Logged in"
+  }
+}
