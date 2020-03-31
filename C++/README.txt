e5b64186181a6be555610f1a4ea345c993ef9dd2
J.B Stepan <tonymoooon543@gmail.com>
2020-03-31 10:43:59 -0500

    Commit

---
+++ C++/README.txt
@@ -0,0 +1,2 @@
+This is where I put my C++ code
+Started to learn on: 3/31/2020
---
+++ C++/testing1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // This prints out the string "Hello World!"
+  cout << "Hello World! \n";
+  // This prints out the int 1
+  cout << 1 << endl;
+  return 0;
+} 
