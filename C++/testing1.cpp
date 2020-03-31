badeea1eec2ed1c68260e9a1cc44cad6f7b9a88f
J.B Stepan <tonymoooon543@gmail.com>
2020-03-31 11:03:56 -0500

    Update

--- C++/testing1.cpp
+++ C++/testing1.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 using namespace std;
 
+// Variables
+int myNum2;
+char myChar = 'C';
+double myFloat = 5.22;
+bool myBool = true;
+int ageOfUser = 18;
+int x = 5;
+int y = -2;
+int cord = x * y;
+
 int main() {
+  int myNum = 15;
+  myNum2 = 2;
   // This prints out the string "Hello World!"
   cout << "Hello World! \n";
   // This prints out the int 1
   cout << 1 << endl;
+  cout << myNum + myNum2 << endl;
+  cout << "I am " << ageOfUser << " years old." << endl;
+  cout << cord << endl;
   return 0;
 } 
