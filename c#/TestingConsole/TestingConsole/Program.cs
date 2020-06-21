c4aeb16165c35af45150ec3e789d156f761dd8dd
J.B Stepan <tonymoooon543@gmail.com>
2020-06-20 22:42:43 -0500

    Add files via upload

---
+++ c#/TestingConsole/TestingConsole/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TestingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Infi loop
+            while (true)
+            {
+                string title1 = "H";
+                string title2 = "He";
+                string title3 = "Hel";
+                string title4 = "Hell";
+                string title5 = "Hello";
+
+                Console.BackgroundColor = ConsoleColor.Blue;
+
+                Console.Title = title1;
+                Thread.Sleep(500);
+                Console.Title = title2;
+                Thread.Sleep(500);
+                Console.Title = title3;
+                Thread.Sleep(500);
+                Console.Title = title4;
+                Thread.Sleep(500);
+                Console.Title = title5;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
