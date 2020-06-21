ce2ef43ea28337f041c6948d680d28503a2d265f
J.B Stepan <tonymoooon543@gmail.com>
2020-06-21 10:46:22 -0500

    Add files via upload

---
+++ OtherLangs/FSharp/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let x = 2 + 6
+    printfn "The Number: %x" x
+    0 // return an integer exit code
