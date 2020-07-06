81b3ff2163a34880c8e66fbf1a49a9e4fb44baae
J.B Stepan <tonymoooon543@gmail.com>
2020-07-05 21:01:48 -0500

    Add files via upload

---
+++ java/variables.java
@@ -0,0 +1,37 @@
+public class variables 
+{ // I dont think is a good formatting, but its the c# in me
+	public static void main()
+	{
+		//-----------------------------------------------
+		// Whole numbers
+		// stores numbers from -128 to 127
+		byte myByte = 127;
+		// stores whole numbers from -32768 to 32767
+		short myShort = 32767;
+		// stores whole number from -2147483648 to 2147483647
+		int myInt = 2147483647;
+		// stores whole number from -9223372036854775808 to 9223372036854775807
+		long myLong = 9223372036854775807L;
+		//------------------------------------------------		
+		// Floating point
+		// stores float from 3.4e−038 to 3.4e+038
+		float myFloat = 3.4e+038f;
+		// stores float from 1.7e−308 to 1.7e+308
+		double myDouble = 1.7e+308d;
+		//-------------------------------------------------		
+		//Scientific Numbers
+		// stores Scientific Numbers
+		float mySn = 1.3e5;
+		//-------------------------------------------------		
+		// booleans
+		boolean myBool = true;		
+		//--------------------------------------------------		
+		//Characters
+		char myChar = 'E';		
+		// -------------------------------------------------
+		// Strings
+		String myString = "Hello world";
+		//--------------------------------------------------
+		System.out.print(myString);
+	}
+}
